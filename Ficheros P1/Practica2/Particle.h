3d08702b30032566e35c6296a04d66c2b39f427f
Jorge Rodríguez García <jorger09@ucm.es>
2018-09-30 14:46:44 +0200

    parte 1 practica 1 hecha y comentada

---
+++ Ficheros P1/Practica2/Particle.h
@@ -0,0 +1,34 @@
+#include "core.hpp"
+#include "RenderUtils.hpp"
+
+#pragma once
+class Particle: public RenderItem      //clase particula, puede dibujarse
+{
+private:
+	Vector3 p;         //posicion
+	Vector3 v;         //velocidad  
+	Vector3 a;         //aceleracion
+	float damping;     //rozamiento
+	int inverse_mass;  //masa inversa. Si es 0 la masa sera infinita
+
+	void integrate(float t)    //actualiza los parametros de particula
+	{
+		if (inverse_mass <= 0.0f) return;   //si la masa es infinita no hace nada
+
+		p += v * t;                         //si no, actualizamos parametros
+		v += a * t;
+		v *= powf(damping, t);
+	}
+public:
+	Particle(physx::PxShape* _shape, Vector3 p_ = { 0.0, 0.0, 0.0 }, Vector3 v_ = { 0.0, 0.0, 0.0 }, 
+		Vector3 a_ = { 0.0, 0.0, 0.0 }, float damping_ = 1.0, int inverse_mass_ = 0) :
+		p(p_), v(v_), a(a_), damping(damping_), inverse_mass(inverse_mass_),
+		RenderItem(_shape, &physx::PxTransform(p), Vector3(1.0, 2.0, 3.0)) {}
+	//la constructora de RenderItem recibe una forma, un transform que engloba una posicion y una orientacion
+	//y un Vector3 para el color
+
+	void update(float t) { integrate(t); }
+
+	~Particle() {}
+};
+
---
+++ Ficheros P1/Practica4/RenderUtils.hpp
@@ -0,0 +1,59 @@
+#ifndef __RENDER_UTILS_H__
+#define __RENDER_UTILS_H__
+
+#include "PxPhysicsAPI.h"
+#include "core.hpp"
+
+class RenderItem;
+void RegisterRenderItem(const RenderItem* _item);
+void DeregisterRenderItem(const RenderItem* _item);
+
+class RenderItem
+{
+public:
+	RenderItem(physx::PxShape* _shape, const physx::PxTransform* _trans, Vector3 _color) :
+		shape(_shape), transform(_trans), color(_color), references(1)
+	{
+		shape->acquireReference();
+		RegisterRenderItem(this);
+	}
+
+	RenderItem(physx::PxShape* _shape, Vector3 _color) :
+		shape(_shape), transform(NULL), color(_color), references(1)
+	{
+		shape->acquireReference();
+		RegisterRenderItem(this);
+	}
+
+	RenderItem() : shape(NULL), transform(NULL), references(1) {}
+
+	void addReference()
+	{
+		++references;
+	}
+
+	void release()
+	{
+		--references;
+		if (references == 0)
+		{
+			DeregisterRenderItem(this);
+			shape->release();
+			delete this;
+		}
+	}
+
+public:
+	physx::PxShape* shape;
+	const physx::PxTransform* transform;
+	Vector3 color;
+
+	unsigned references;
+};
+
+double GetLastTime();
+Camera* GetCamera();
+
+physx::PxShape* CreateShape(const physx::PxGeometry& geo);
+
+#endif
